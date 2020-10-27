2bebdd6b9712d5ded386f4fd0cfb957b12abf3e6
aarthiganesh55 <61809010+aarthiganesh55@users.noreply.github.com>
2020-10-27 12:43:04 -0400

    changes

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -17,6 +17,10 @@ function sortFunction(a, b, key) {
   return 0;
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 document.body.addEventListener('submit', async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
@@ -30,7 +34,32 @@ document.body.addEventListener('submit', async (e) => {
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
       // You're going to do your lab work in here. Replace this comment.
-      console.log('fromServer', fromServer);
+      // console.log('fromServer', fromServer);
+      const countries = range(10).map((i) => fromServer[getRandomInt(fromServer.length)]);
+      countries.sort((b,a) => sortFunction(a,b, 'name'));
+      console.log(countries)
+
+      const order = document.createElement('ol');
+      order.setAttribute('class', 'flex-inner');
+      countries.map((country) => {
+        const unorder = document.createElement('li');
+        const check = document.createElement('input');
+        check.setAttribute('type','checkbox');
+        check.setAttribute('id',country.code);
+        check.value = country.code;
+        check.name = 'aarthi';
+        const label = document.createElement('label');
+        label.setAttribute('for',country.code);
+        label.innerText = country.name;
+        unorder.appendChild(check);
+        unorder.appendChild(label);
+        order.appendChild(unorder);
+      });
+
+      const container = document.getElementById('checklistCont');
+      container.innerHTML ='';
+      container.appendChild(order)
     })
+
     .catch((err) => console.log(err));
 });
